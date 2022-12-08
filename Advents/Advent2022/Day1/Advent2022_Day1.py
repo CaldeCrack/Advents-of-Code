da6cc7355273b8215f58f0ee5a26f2ac6d6553fa
Andrés Calderón <an.caldeguardia@gmail.com>
2022-12-08 00:36:56 -0300

    Fixed Day1 and updated README

--- Advents/Advent2022/Day1/Advent2022_Day1.py
+++ Advents/Advent2022/Day1/Advent2022_Day1.py
@@ -1,9 +1,7 @@
 # First Star
-data = open("C:/Users/ninja/OneDrive/Escritorio/Cosas de la U/Otros/Scripts/Advent2022/Day1/data.txt", "r")
-newData = data.readlines()
-calories = []
+data = open("data.txt", "r") # no care about closing the file
+newData, calories, n = data.readlines(), [], 0
 
-n = 0
 for line in newData:
     if line == "\n" or line == "":
         calories.append(n)
@@ -11,8 +9,6 @@ for line in newData:
         continue
     n += int(line[:-1])
 
-print(max(calories))
-
 # Second Star
 calories.sort()
-print(calories[-3]+calories[-2]+calories[-1])
+print("max:" , str(calories[-1]) + ", sum of the highest three:" , str(sum(calories[-3:])))
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # advents-of-code
- My solutions to the advents of code
+
+ My solutions to the advents of code in Python 3.10.4
