7737392f684132097622eafcdb4d8c7976b976df
Andrés Calderón <an.caldeguardia@gmail.com>
2022-12-08 03:18:42 -0300

    Minor fix

--- Advents/Advent2022/Day3/Day3.py
+++ Advents/Advent2022/Day3/Day3.py
@@ -4,10 +4,9 @@ newData = data.readlines()
 rucksacks = [[rucksack.strip()[:(((len(rucksack)-2)//2)+1)], rucksack.strip()[(len(rucksack)//2):]] for rucksack in newData]
 matches = [[item for item in compartments[0] if item in compartments[1]] for compartments in rucksacks]
 priorities = [ord(item[0])-96 if ord(item[0])>96 else ord(item[0])-38 for item in matches]
-print(sum(priorities))
 
 #Second Star
 rucksacks = [newData[x:x+3] for x in range(0, len(newData), 3)]
 matches = [[item for item in compartments[0].strip() if (item in compartments[1].strip() and item in compartments[2].strip())] for compartments in rucksacks]
-priorities = [ord(item[0])-96 if ord(item[0])>96 else ord(item[0])-38 for item in matches]
-print(sum(priorities))
+priorities2 = [ord(item[0])-96 if ord(item[0])>96 else ord(item[0])-38 for item in matches]
+print("sum of first priorities:" , str(sum(priorities)) + ", sum of second priorities:" , str(sum(priorities2)))
